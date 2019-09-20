2f06e66241af25e85db62f8b8b5e985ed14df003
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2019-09-20 11:54:08 +0100

    [CREATE] False-Positives

--- src/constants/countings.py
+++ src/constants/countings.py
@@ -63,29 +63,29 @@ from classifications import *
 #             Singles            #
 # ============================== #
 
-cltp_0_0 = countLowTruePositives(0, 0, df_birads_real, df_birads_phy)
-cmtp_0_0 = countMedTruePositives(0, 0, df_birads_real, df_birads_phy)
-chtp_0_0 = countHghTruePositives(0, 0, df_birads_real, df_birads_phy)
+cltp_0_0 = countLowTruePositives(0, 0, df_birads_phy, df_birads_real)
+cmtp_0_0 = countMedTruePositives(0, 0, df_birads_phy, df_birads_real)
+chtp_0_0 = countHghTruePositives(0, 0, df_birads_phy, df_birads_real)
 
-cltp_1_1 = countLowTruePositives(1, 1, df_birads_real, df_birads_phy)
-cmtp_1_1 = countMedTruePositives(1, 1, df_birads_real, df_birads_phy)
-chtp_1_1 = countHghTruePositives(1, 1, df_birads_real, df_birads_phy)
+cltp_1_1 = countLowTruePositives(1, 1, df_birads_phy, df_birads_real)
+cmtp_1_1 = countMedTruePositives(1, 1, df_birads_phy, df_birads_real)
+chtp_1_1 = countHghTruePositives(1, 1, df_birads_phy, df_birads_real)
 
-cltp_2_2 = countLowTruePositives(2, 2, df_birads_real, df_birads_phy)
-cmtp_2_2 = countMedTruePositives(2, 2, df_birads_real, df_birads_phy)
-chtp_2_2 = countHghTruePositives(2, 2, df_birads_real, df_birads_phy)
+cltp_2_2 = countLowTruePositives(2, 2, df_birads_phy, df_birads_real)
+cmtp_2_2 = countMedTruePositives(2, 2, df_birads_phy, df_birads_real)
+chtp_2_2 = countHghTruePositives(2, 2, df_birads_phy, df_birads_real)
 
-cltp_3_3 = countLowTruePositives(3, 3, df_birads_real, df_birads_phy)
-cmtp_3_3 = countMedTruePositives(3, 3, df_birads_real, df_birads_phy)
-chtp_3_3 = countHghTruePositives(3, 3, df_birads_real, df_birads_phy)
+cltp_3_3 = countLowTruePositives(3, 3, df_birads_phy, df_birads_real)
+cmtp_3_3 = countMedTruePositives(3, 3, df_birads_phy, df_birads_real)
+chtp_3_3 = countHghTruePositives(3, 3, df_birads_phy, df_birads_real)
 
-cltp_4_4 = countLowTruePositives(4, 4, df_birads_real, df_birads_phy)
-cmtp_4_4 = countMedTruePositives(4, 4, df_birads_real, df_birads_phy)
-chtp_4_4 = countHghTruePositives(4, 4, df_birads_real, df_birads_phy)
+cltp_4_4 = countLowTruePositives(4, 4, df_birads_phy, df_birads_real)
+cmtp_4_4 = countMedTruePositives(4, 4, df_birads_phy, df_birads_real)
+chtp_4_4 = countHghTruePositives(4, 4, df_birads_phy, df_birads_real)
 
-cltp_5_5 = countLowTruePositives(5, 5, df_birads_real, df_birads_phy)
-cmtp_5_5 = countMedTruePositives(5, 5, df_birads_real, df_birads_phy)
-chtp_5_5 = countHghTruePositives(5, 5, df_birads_real, df_birads_phy)
+cltp_5_5 = countLowTruePositives(5, 5, df_birads_phy, df_birads_real)
+cmtp_5_5 = countMedTruePositives(5, 5, df_birads_phy, df_birads_real)
+chtp_5_5 = countHghTruePositives(5, 5, df_birads_phy, df_birads_real)
 
 # ============================== #
 
@@ -126,6 +126,128 @@ c_5_5 = cltp_5_5 + cmtp_5_5 + chtp_5_5
 # ============================== #
 # ============================== #
 
+# ============================== #
+# ============================== #
+#    COUNTING FALSE-POSITIVES    #
+# ============================== #
+# ============================== #
+
+# ============================== #
+#             Singles            #
+# ============================== #
+
+clfp_1_0 = countLowFalsePositives(1, 0, df_birads_phy, df_birads_real)
+cmfp_1_0 = countMedFalsePositives(1, 0, df_birads_phy, df_birads_real)
+chfp_1_0 = countHghFalsePositives(1, 0, df_birads_phy, df_birads_real)
+
+clfp_2_0 = countLowFalsePositives(2, 0, df_birads_phy, df_birads_real)
+cmfp_2_0 = countMedFalsePositives(2, 0, df_birads_phy, df_birads_real)
+chfp_2_0 = countHghFalsePositives(2, 0, df_birads_phy, df_birads_real)
+
+clfp_3_0 = countLowFalsePositives(3, 0, df_birads_phy, df_birads_real)
+cmfp_3_0 = countMedFalsePositives(3, 0, df_birads_phy, df_birads_real)
+chfp_3_0 = countHghFalsePositives(3, 0, df_birads_phy, df_birads_real)
+
+clfp_4_0 = countLowFalsePositives(4, 0, df_birads_phy, df_birads_real)
+cmfp_4_0 = countMedFalsePositives(4, 0, df_birads_phy, df_birads_real)
+chfp_4_0 = countHghFalsePositives(4, 0, df_birads_phy, df_birads_real)
+
+clfp_5_0 = countLowFalsePositives(5, 0, df_birads_phy, df_birads_real)
+cmfp_5_0 = countMedFalsePositives(5, 0, df_birads_phy, df_birads_real)
+chfp_5_0 = countHghFalsePositives(5, 0, df_birads_phy, df_birads_real)
+
+clfp_2_1 = countLowFalsePositives(2, 1, df_birads_phy, df_birads_real)
+cmfp_2_1 = countMedFalsePositives(2, 1, df_birads_phy, df_birads_real)
+chfp_2_1 = countHghFalsePositives(2, 1, df_birads_phy, df_birads_real)
+
+clfp_3_1 = countLowFalsePositives(3, 1, df_birads_phy, df_birads_real)
+cmfp_3_1 = countMedFalsePositives(3, 1, df_birads_phy, df_birads_real)
+chfp_3_1 = countHghFalsePositives(3, 1, df_birads_phy, df_birads_real)
+
+clfp_4_1 = countLowFalsePositives(4, 1, df_birads_phy, df_birads_real)
+cmfp_4_1 = countMedFalsePositives(4, 1, df_birads_phy, df_birads_real)
+chfp_4_1 = countHghFalsePositives(4, 1, df_birads_phy, df_birads_real)
+
+clfp_5_1 = countLowFalsePositives(5, 1, df_birads_phy, df_birads_real)
+cmfp_5_1 = countMedFalsePositives(5, 1, df_birads_phy, df_birads_real)
+chfp_5_1 = countHghFalsePositives(5, 1, df_birads_phy, df_birads_real)
+
+clfp_3_2 = countLowFalsePositives(3, 2, df_birads_phy, df_birads_real)
+cmfp_3_2 = countMedFalsePositives(3, 2, df_birads_phy, df_birads_real)
+chfp_3_2 = countHghFalsePositives(3, 2, df_birads_phy, df_birads_real)
+
+clfp_4_2 = countLowFalsePositives(4, 2, df_birads_phy, df_birads_real)
+cmfp_4_2 = countMedFalsePositives(4, 2, df_birads_phy, df_birads_real)
+chfp_4_2 = countHghFalsePositives(4, 2, df_birads_phy, df_birads_real)
+
+clfp_5_2 = countLowFalsePositives(5, 2, df_birads_phy, df_birads_real)
+cmfp_5_2 = countMedFalsePositives(5, 2, df_birads_phy, df_birads_real)
+chfp_5_2 = countHghFalsePositives(5, 2, df_birads_phy, df_birads_real)
+
+clfp_4_3 = countLowFalsePositives(4, 3, df_birads_phy, df_birads_real)
+cmfp_4_3 = countMedFalsePositives(4, 3, df_birads_phy, df_birads_real)
+chfp_4_3 = countHghFalsePositives(4, 3, df_birads_phy, df_birads_real)
+
+clfp_5_3 = countLowFalsePositives(5, 3, df_birads_phy, df_birads_real)
+cmfp_5_3 = countMedFalsePositives(5, 3, df_birads_phy, df_birads_real)
+chfp_5_3 = countHghFalsePositives(5, 3, df_birads_phy, df_birads_real)
+
+clfp_5_4 = countLowFalsePositives(5, 4, df_birads_phy, df_birads_real)
+cmfp_5_4 = countMedFalsePositives(5, 4, df_birads_phy, df_birads_real)
+chfp_5_4 = countHghFalsePositives(5, 4, df_birads_phy, df_birads_real)
+
+# ============================== #
+
+# ============================== #
+#             Totals             #
+# ============================== #
+
+c_1_0 = clfp_1_0 + cmfp_1_0 + chfp_1_0
+c_2_0 = clfp_2_0 + cmfp_2_0 + chfp_2_0
+c_3_0 = clfp_3_0 + cmfp_3_0 + chfp_3_0
+c_4_0 = clfp_4_0 + cmfp_4_0 + chfp_4_0
+c_5_0 = clfp_5_0 + cmfp_5_0 + chfp_5_0
+
+c_2_1 = clfp_2_1 + cmfp_2_1 + chfp_2_1
+c_3_1 = clfp_3_1 + cmfp_3_1 + chfp_3_1
+c_4_1 = clfp_4_1 + cmfp_4_1 + chfp_4_1
+c_5_1 = clfp_5_1 + cmfp_5_1 + chfp_5_1
+
+c_3_2 = clfp_3_2 + cmfp_3_2 + chfp_3_2
+c_4_2 = clfp_4_2 + cmfp_4_2 + chfp_4_2
+c_5_2 = clfp_5_2 + cmfp_5_2 + chfp_5_2
+
+c_4_3 = clfp_4_3 + cmfp_4_3 + chfp_4_3
+c_5_3 = clfp_5_3 + cmfp_5_3 + chfp_5_3
+
+c_5_4 = clfp_5_4 + cmfp_5_4 + chfp_5_4
+
+# ============================== #
+
+# ============================== #
+# ============================== #
+
+# ============================== #
+# ============================== #
+#    COUNTING FALSE-NEGATIVES    #
+# ============================== #
+# ============================== #
+
+# ============================== #
+#             Singles            #
+# ============================== #
+
+# ============================== #
+
+# ============================== #
+#             Totals             #
+# ============================== #
+
+# ============================== #
+
+# ============================== #
+# ============================== #
+
 c_0_1 = 1
 c_0_2 = 1
 c_0_3 = 1
@@ -142,7 +264,6 @@ c_1_4 = 1
 c_1_5 = 1
 
 c_2_0 = 1
-c_2_1 = 1
 
 c_2_2 = cltp_2_2 + cmtp_2_2 + chtp_2_2
 
@@ -151,8 +272,6 @@ c_2_4 = 1
 c_2_5 = 1
 
 c_3_0 = 1
-c_3_1 = 1
-c_3_2 = 1
 
 c_3_3 = cltp_3_3 + cmtp_3_3 + chtp_3_3
 
@@ -160,19 +279,12 @@ c_3_4 = 1
 c_3_5 = 1
 
 c_4_0 = 1
-c_4_1 = 1
-c_4_2 = 1
-c_4_3 = 1
 
 c_4_4 = cltp_4_4 + cmtp_4_4 + chtp_4_4
 
 c_4_5 = 1
 
 c_5_0 = 1
-c_5_1 = 1
-c_5_2 = 1
-c_5_3 = 1
-c_5_4 = 1
 
 c_5_5 = cltp_5_5 + cmtp_5_5 + chtp_5_5
 
